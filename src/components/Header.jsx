dba36ba71b1dc6dd6826c09cbe04d99efee0089e
Hamed Samavati <hamedsamavaty@gmail.com>
2025-03-01 20:06:20 -0600

    heading style works good

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,20 @@ import React from "react";
 import Navbar from "./Navbar";
 import { PiSunDimLight } from "react-icons/pi";
 import { Link } from "react-router-dom";
+import { FaChevronDown } from "react-icons/fa";
 import "../styles/Header.scss";
+
+import { useState } from "react";
+
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedLang, setSelectedLang] = useState("En");
+
+  const languages = [
+    { value: "English", label: "En" },
+    { value: "French", label: "Fr" },
+    { value: "Persian", label: "Fa" },
+  ];
   return (
     <>
       <header>
@@ -14,14 +26,26 @@ function Header() {
         <button className="contact-button">
           <Link to="/contact">Contact</Link>
         </button>
-        <select name="language" id="language">
-          <option value="English" defaultValue>
-            En
-          </option>
-          <option value="French">Fr</option>
-          <option value="Persian">Fa</option>
-        </select>
-        <div>
+        <div className="custom-select" onClick={() => setIsOpen(!isOpen)}>
+          <span>
+            {selectedLang}
+            <FaChevronDown />
+          </span>
+          {isOpen && (
+            <div className="select-options">
+              {languages.map((lang) => (
+                <div
+                  key={lang.value}
+                  className="option"
+                  onClick={() => setSelectedLang(lang.label)}
+                >
+                  {lang.label}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+        <div className="login-register-buttons">
           <button className="login-button">Login</button>
           <button className="register-button">Register</button>
         </div>
